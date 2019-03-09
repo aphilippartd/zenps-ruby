5c552daa15dc5f1adf9d9e4d9b2c82e393542b8d
Alexis Philippart de Foy <alexis.philippartdefoy@gmail.com>
2019-03-09 22:16:35 +0100

    Adapt specs to include first and last name to subject input(s)

--- spec/zenps/survey_spec.rb
+++ spec/zenps/survey_spec.rb
@@ -5,14 +5,21 @@ describe Zenps::Survey do
   it 'performs request for mixed inputs' do
     expect_any_instance_of(Zenps::Client).to receive(:call).with(hash_including(email: 'john.doe.1@acme.com'))
     expect_any_instance_of(Zenps::Client).to receive(:call).with(hash_including(email: 'john.doe.2@acme.com', locale: 'nl'))
-    expect_any_instance_of(Zenps::Client).to receive(:call).with(hash_including(email: 'john.doe.3@acme.com', locale: 'fr'))
+    expect_any_instance_of(Zenps::Client)
+      .to receive(:call)
+      .with(hash_including(email: 'john.doe.3@acme.com', locale: 'fr', first_name: 'John', last_name: 'Doe'))
 
     Zenps.configure(zenps_key: 'VALID_ZENPS_KEY')
 
     input = [
       'john.doe.1@acme.com',
       { email: 'john.doe.2@acme.com', locale: 'nl' },
-      JSON.parse({ email: 'john.doe.3@acme.com', locale: 'fr' }.to_json, object_class: OpenStruct)
+      JSON.parse({
+        email: 'john.doe.3@acme.com',
+        locale: 'fr',
+        first_name: 'John',
+        last_name: 'Doe'
+      }.to_json, object_class: OpenStruct)
     ]
 
     subject.call(input)
@@ -21,7 +28,9 @@ describe Zenps::Survey do
   it 'performs request for with options' do
     body = {
       to: {
-        email: 'john.doe.1@acme.com'
+        email: 'john.doe.1@acme.com',
+        first_name: 'John',
+        last_name: 'Doe'
       },
       locale: 'nl',
       event: 'sign_up',
@@ -32,7 +41,7 @@ describe Zenps::Survey do
 
     Zenps.configure(zenps_key: 'VALID_ZENPS_KEY')
 
-    input = ['john.doe.1@acme.com']
+    input = { email: 'john.doe.1@acme.com', first_name: 'John', last_name: 'Doe' }
 
     subject.call(input, locale: 'nl', event: 'sign_up', tags: %w[man facebook])
   end
